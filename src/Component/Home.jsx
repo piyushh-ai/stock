a8b61ffbd7aa110951382f8cb4f05b8dcdb4b218
piyushh-ai <thepiyushsirolia@gmail.com>
2026-01-27 16:52:37 +0530

    marqueee

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -3,17 +3,14 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="min-h-screen w-full flex items-center justify-center bg-gradient-to-br from-slate-50 to-slate-200 px-6">
-      <div className="w-full max-w-md bg-white rounded-3xl shadow-xl p-8 flex flex-col gap-10">
-        
+    <div className="min-h-screen w-full flex flex-col items-center justify-center bg-gradient-to-br from-slate-50 to-slate-200 ">
+      <div className="w-[92%] max-w-md bg-white rounded-3xl shadow-xl p-8 flex flex-col gap-10 ">
         {/* Header */}
         <div className="text-center">
           <h1 className="text-3xl font-semibold tracking-wide">
             B.K Engineering
           </h1>
-          <p className="text-gray-500 text-lg mt-1">
-            Inventory Management
-          </p>
+          <p className="text-gray-500 text-lg mt-1">Inventory Management</p>
         </div>
 
         {/* Content */}
@@ -47,6 +44,27 @@ const Home = () => {
         <p className="text-center text-sm text-gray-400">
           Discipline. Precision. Control.
         </p>
+      </div>
+
+      <div className="w-full mt-14 overflow-hidden">
+        <div className="marquee">
+          <div className="marquee-track">
+            <img className="logo" src="/public/company_images/Bosch_logo.png" />
+            <img className="logo" src="/public/company_images/lucas.png" />
+            <img className="logo" src="/public/company_images/nbc.png" />
+            <img className="logo" src="/public/company_images/rmp.png" />
+            <img className="logo" src="/public/company_images/delphi_tvs.png" />
+            <img className="logo" src="/public/company_images/ascot1.png" />
+
+            {/* duplicate – SAME track me */}
+            <img className="logo" src="/public/company_images/Bosch_logo.png" />
+            <img className="logo" src="/public/company_images/lucas.png" />
+            <img className="logo" src="/public/company_images/nbc.png" />
+            <img className="logo" src="/public/company_images/rmp.png" />
+            <img className="logo" src="/public/company_images/delphi_tvs.png" />
+            <img className="logo" src="/public/company_images/ascot1.png" />
+          </div>
+        </div>
       </div>
     </div>
   );
