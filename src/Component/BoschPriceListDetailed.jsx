7ce3eb3eff1074d26aba699dc348fadd57fc6066
piyushh-ai <thepiyushsirolia@gmail.com>
2026-01-29 23:35:32 +0530

    price

--- src/Component/BoschPriceListDetailed.jsx
+++ src/Component/BoschPriceListDetailed.jsx
@@ -12,24 +12,24 @@ const BoschPriceListDetailed = () => {
     <div className="flex flex-col items-center justify-center py-10">
       <div className="w-[92%] max-w-md bg-white rounded-4xl shadow-[0_20px_60px_rgba(0,0,0,0.08)] px-8 py-10 flex flex-col gap-10 items-center">
         <h1 className="text-xl font-bold tracking-wide">Item description</h1>
-        <div className="w-full px-3 flex flex-col items-start gap-3">
-          <h1 className="text-lg">
+        <div className="w-full  flex flex-col items-start gap-5">
+          <h1 className="text-md">
             Part No. - {" "}
             <span className="px-1 tracking-widest text-blue-700">{item.part}</span>
           </h1>
-          <h1 className="text-lg">
+          <h1 className="text-md">
             Description - {" "}
             <span className="px-1 tracking-widest">{item.desc}</span>
           </h1>
-          <h1 className="text-lg">
+          <h1 className="text-md">
             Current Mrp - {" "}
             <span className="px-1 tracking-widest">{item.mrp}/-</span>
           </h1>
-          <h1 className="text-lg">
+          <h1 className="text-md">
             HSN Code - {" "}
             <span className="px-1 tracking-widest">{item.hsn}</span>
           </h1>
-          <h1 className="text-lg">
+          <h1 className="text-md">
             GST - {" "}
             <span className="px-1 tracking-widest">{item.gst}%</span>
           </h1>
