a1eebfacf34f3613c42bad272c3b2830c6f961ed
piyushh-ai <thepiyushsirolia@gmail.com>
2026-01-30 18:23:36 +0530

    price

--- src/Context/BoschPriceListContext.jsx
+++ src/Context/BoschPriceListContext.jsx
@@ -19,25 +19,40 @@ const BoschPriceListContext = (props) => {
   
 
   useEffect(() => {
-    let finalData = [];
-    const readExcel = async () => {
-      const res = await axios.get("/price_lists/PRICE_LIST_PC_JAN_2026.xlsx", {
+  let finalData = [];
+
+  const readExcel = async () => {
+    setLoading(true);
+
+    const files = [
+      "/price_lists/PRICE_LIST_PC_JAN_2026.xlsx",
+      "/price_lists/Price_Report_Bosch_cv_oct.xlsx",
+    ];
+
+    for (let filePath of files) {
+      const res = await axios.get(filePath, {
         responseType: "arraybuffer",
       });
+
       const buffer = res.data;
       const workbook = XLSX.read(buffer, { type: "array" });
       const sheetName = workbook.SheetNames[0];
-      const rawDate = workbook.Props.ModifiedDate;
-      if (rawDate) {
-        const formatted = rawDate.toString().split(" GMT")[0];
+
+      // modified date sirf pehli file se uthayenge
+      if (!modifiedOn && workbook.Props?.ModifiedDate) {
+        const formatted = workbook.Props.ModifiedDate
+          .toString()
+          .split(" GMT")[0];
         setModifiedOn(formatted);
       }
+
       const worksheet = workbook.Sheets[sheetName];
       const rows = XLSX.utils.sheet_to_json(worksheet, {
         header: 1,
         defval: "",
         blankrows: false,
       });
+
       for (let i = 1; i < rows.length; i++) {
         const row = rows[i];
         if (!row[1]) continue;
@@ -57,12 +72,15 @@ const BoschPriceListContext = (props) => {
           sheetN: normalize(sheetName),
         });
       }
-      setAllData(finalData);
-setLoading(false);
+    }
 
-    };
-    readExcel();
-  }, []);
+    setAllData(finalData);
+    setLoading(false);
+  };
+
+  readExcel();
+}, []);
+
 
   return (
     <boschPricePC.Provider value={[allData, setAllData, modifiedOn, loading]}>
